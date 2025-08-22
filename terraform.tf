de78ba00081e0f3de7ce85082106f691b7558e84
srojas <fesaroma@outlook.com>
2025-08-22 17:43:49 -0600

    lab-aws-lambda

--- terraform.tf
+++ terraform.tf
@@ -1,11 +1,17 @@
 terraform {
+  # Versión de Terraform CLI que se permite usar
+  required_version = "1.13"
 
+  # Providers y sus versiones
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "6.9.0"
     }
+    # Para crear archivos comprimidos (.zip)
+    archive = {
+      source  = "hashicorp/archive"
+      version = "2.7.1"
+    }
   }
-
-  required_version = "~> 1.13"
 }
